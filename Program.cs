1d1907177690b4f38b41e776e9cae7ae7e85b5b4
tarana <beshirova@hotmail.com>
2022-11-14 23:02:22 +0300

    Объявляем функцию foreach для перебора всех элементов массива

--- Program.cs
+++ Program.cs
@@ -7,4 +7,11 @@
             var result = new string[Array .Length];
             var values = 0;
             
-           
+           foreach (var value in Array )
+            {
+                if (value.Length <= 3)
+                {
+                    result[values] = value;
+                    values++;
+                }
+            }
