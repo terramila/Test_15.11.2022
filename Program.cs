74549765e98c9f19d0a210e6f6370a2c80bb6afe
tarana <beshirova@hotmail.com>
2022-11-14 22:55:06 +0300

    Формируем массив из заданных элементов

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿
+   string[] Array;
+            Console.WriteLine("Введите массив строк через пробел");
+            string stringUser = Console.ReadLine();
+            Array = stringUser.Split(' ');
+           
